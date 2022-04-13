882e53b06cc4e2c6fc18f84737389d02a55646a6
Romain <piccolo.rom@gmail.com>
2022-04-13 16:45:31 +0200

    Footer OK

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,13 @@
-import Logo from '../Logo'
+import styles from './footer.module.css';
+import Logo from '../Logo';
 
 function Footer() {
     return (
-        <div>
+        <div className={styles.footer}>
             <Logo darkmode />
+            <div>© 2020 Kasa. All rights reserved</div>
         </div>
-    )
+    );
 }
 
 export default Footer;
